a35b6ed4ae63c99eae78e361faeafa7ab3ffaf65
Luca <1058086@bkrnet.de>
2022-12-08 09:20:25 +0100

    Added Everything

---
+++ forms_lotto/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace forms_lotto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.b_roll = new System.Windows.Forms.Button();
+            this.l_luckynum1 = new System.Windows.Forms.Label();
+            this.l_luckynum2 = new System.Windows.Forms.Label();
+            this.l_luckynum3 = new System.Windows.Forms.Label();
+            this.l_luckynum4 = new System.Windows.Forms.Label();
+            this.l_luckynum5 = new System.Windows.Forms.Label();
+            this.l_luckynum6 = new System.Windows.Forms.Label();
+            this.t_entrynum1 = new System.Windows.Forms.TextBox();
+            this.t_entrynum2 = new System.Windows.Forms.TextBox();
+            this.t_entrynum3 = new System.Windows.Forms.TextBox();
+            this.t_entrynum4 = new System.Windows.Forms.TextBox();
+            this.t_entrynum5 = new System.Windows.Forms.TextBox();
+            this.t_entrynum6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.title.Location = new System.Drawing.Point(269, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(246, 40);
+            this.title.TabIndex = 0;
+            this.title.Text = "Gamble-Inator";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.title.UseWaitCursor = true;
+            // 
+            // b_roll
+            // 
+            this.b_roll.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b_roll.Location = new System.Drawing.Point(276, 116);
+            this.b_roll.Name = "b_roll";
+            this.b_roll.Size = new System.Drawing.Size(239, 119);
+            this.b_roll.TabIndex = 1;
+            this.b_roll.Text = "Gamble Time!";
+            this.b_roll.UseVisualStyleBackColor = true;
+            this.b_roll.UseWaitCursor = true;
+            this.b_roll.Click += new System.EventHandler(this.b_roll_Click);
+            // 
+            // l_luckynum1
+            // 
+            this.l_luckynum1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.l_luckynum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.l_luckynum1.Location = new System.Drawing.Point(200, 275);
+            this.l_luckynum1.Name = "l_luckynum1";
+            this.l_luckynum1.Size = new System.Drawing.Size(60, 60);
+            this.l_luckynum1.TabIndex = 2;
+            this.l_luckynum1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.l_luckynum1.UseWaitCursor = true;
+            // 
+            // l_luckynum2
+            // 
+            this.l_luckynum2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.l_luckynum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.l_luckynum2.Location = new System.Drawing.Point(266, 275);
+            this.l_luckynum2.Name = "l_luckynum2";
+            this.l_luckynum2.Size = new System.Drawing.Size(60, 60);
+            this.l_luckynum2.TabIndex = 3;
+            this.l_luckynum2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.l_luckynum2.UseWaitCursor = true;
+            // 
+            // l_luckynum3
+            // 
+            this.l_luckynum3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.l_luckynum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.l_luckynum3.Location = new System.Drawing.Point(332, 275);
+            this.l_luckynum3.Name = "l_luckynum3";
+            this.l_luckynum3.Size = new System.Drawing.Size(60, 60);
+            this.l_luckynum3.TabIndex = 4;
+            this.l_luckynum3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.l_luckynum3.UseWaitCursor = true;
+            // 
+            // l_luckynum4
+            // 
+            this.l_luckynum4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.l_luckynum4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.l_luckynum4.Location = new System.Drawing.Point(398, 275);
+            this.l_luckynum4.Name = "l_luckynum4";
+            this.l_luckynum4.Size = new System.Drawing.Size(60, 60);
+            this.l_luckynum4.TabIndex = 5;
+            this.l_luckynum4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.l_luckynum4.UseWaitCursor = true;
+            // 
+            // l_luckynum5
+            // 
+            this.l_luckynum5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.l_luckynum5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.l_luckynum5.Location = new System.Drawing.Point(464, 275);
+            this.l_luckynum5.Name = "l_luckynum5";
+            this.l_luckynum5.Size = new System.Drawing.Size(60, 60);
+            this.l_luckynum5.TabIndex = 6;
+            this.l_luckynum5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.l_luckynum5.UseWaitCursor = true;
+            // 
+            // l_luckynum6
+            // 
+            this.l_luckynum6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.l_luckynum6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.l_luckynum6.Location = new System.Drawing.Point(530, 275);
+            this.l_luckynum6.Name = "l_luckynum6";
+            this.l_luckynum6.Size = new System.Drawing.Size(60, 60);
+            this.l_luckynum6.TabIndex = 7;
+            this.l_luckynum6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.l_luckynum6.UseWaitCursor = true;
+            // 
+            // t_entrynum1
+            // 
+            this.t_entrynum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.t_entrynum1.Location = new System.Drawing.Point(200, 366);
+            this.t_entrynum1.Name = "t_entrynum1";
+            this.t_entrynum1.Size = new System.Drawing.Size(60, 37);
+            this.t_entrynum1.TabIndex = 8;
+            // 
+            // t_entrynum2
+            // 
+            this.t_entrynum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.t_entrynum2.Location = new System.Drawing.Point(266, 366);
+            this.t_entrynum2.Name = "t_entrynum2";
+            this.t_entrynum2.Size = new System.Drawing.Size(60, 37);
+            this.t_entrynum2.TabIndex = 9;
+            // 
+            // t_entrynum3
+            // 
+            this.t_entrynum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.t_entrynum3.Location = new System.Drawing.Point(332, 366);
+            this.t_entrynum3.Name = "t_entrynum3";
+            this.t_entrynum3.Size = new System.Drawing.Size(60, 37);
+            this.t_entrynum3.TabIndex = 10;
+            // 
+            // t_entrynum4
+            // 
+            this.t_entrynum4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.t_entrynum4.Location = new System.Drawing.Point(398, 366);
+            this.t_entrynum4.Name = "t_entrynum4";
+            this.t_entrynum4.Size = new System.Drawing.Size(60, 37);
+            this.t_entrynum4.TabIndex = 11;
+            // 
+            // t_entrynum5
+            // 
+            this.t_entrynum5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.t_entrynum5.Location = new System.Drawing.Point(464, 366);
+            this.t_entrynum5.Name = "t_entrynum5";
+            this.t_entrynum5.Size = new System.Drawing.Size(60, 37);
+            this.t_entrynum5.TabIndex = 12;
+            // 
+            // t_entrynum6
+            // 
+            this.t_entrynum6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.t_entrynum6.Location = new System.Drawing.Point(530, 366);
+            this.t_entrynum6.Name = "t_entrynum6";
+            this.t_entrynum6.Size = new System.Drawing.Size(60, 37);
+            this.t_entrynum6.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.t_entrynum6);
+            this.Controls.Add(this.t_entrynum5);
+            this.Controls.Add(this.t_entrynum4);
+            this.Controls.Add(this.t_entrynum3);
+            this.Controls.Add(this.t_entrynum2);
+            this.Controls.Add(this.t_entrynum1);
+            this.Controls.Add(this.l_luckynum6);
+            this.Controls.Add(this.l_luckynum5);
+            this.Controls.Add(this.l_luckynum4);
+            this.Controls.Add(this.l_luckynum3);
+            this.Controls.Add(this.l_luckynum2);
+            this.Controls.Add(this.l_luckynum1);
+            this.Controls.Add(this.b_roll);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button b_roll;
+        private System.Windows.Forms.Label l_luckynum1;
+        private System.Windows.Forms.Label l_luckynum2;
+        private System.Windows.Forms.Label l_luckynum3;
+        private System.Windows.Forms.Label l_luckynum4;
+        private System.Windows.Forms.Label l_luckynum5;
+        private System.Windows.Forms.Label l_luckynum6;
+        private System.Windows.Forms.TextBox t_entrynum1;
+        private System.Windows.Forms.TextBox t_entrynum2;
+        private System.Windows.Forms.TextBox t_entrynum3;
+        private System.Windows.Forms.TextBox t_entrynum4;
+        private System.Windows.Forms.TextBox t_entrynum5;
+        private System.Windows.Forms.TextBox t_entrynum6;
+    }
+}
+
---
+++ forms_lotto/Form1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forms_lotto
+{
+    public partial class Form1 : Form
+    {
+        //setup Variables
+        int[] lucky_nums = new int[6];
+        Label[] r_labels = new Label[6];
+        TextBox[] choose_textbox = new TextBox[6];
+
+        //setup Randomiser
+        Random rnd = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b_roll_Click(object sender, EventArgs e)
+        {
+            //give each random number Lable an Lable array for later use in the Loop
+            r_labels[0] = l_luckynum1;
+            r_labels[1] = l_luckynum2;
+            r_labels[2] = l_luckynum3;
+            r_labels[3] = l_luckynum4;
+            r_labels[4] = l_luckynum5;
+            r_labels[5] = l_luckynum6;
+
+            //give each written number Lable an array for later use in the Loop
+            choose_textbox[0] = t_entrynum1;
+            choose_textbox[1] = t_entrynum2;
+            choose_textbox[2] = t_entrynum3;
+            choose_textbox[3] = t_entrynum4;
+            choose_textbox[4] = t_entrynum5;
+            choose_textbox[5] = t_entrynum6;
+
+            //give each Lable a Random Number
+            for (int i = 0; i < 6; i++)
+            {
+                
+                lucky_nums[i] = rnd.Next(1, 50);
+                for(int j = 0; j < 6; j++)
+                {            
+                    if (lucky_nums[i] == lucky_nums[j] && i != j && i != 0)
+                    {
+                        i--;
+                    }
+                    r_labels[i].Text = lucky_nums[i].ToString();
+                }
+            }
+            for(int i = 0; i < 6; i++)
+            {
+                bool colorLock = false;
+                for (int j = 0; j < 6; j++)
+                {
+                    if (choose_textbox[i].Text == r_labels[j].Text && choose_textbox[i].Text != "")
+                    {
+                        choose_textbox[i].BackColor = Color.Green;
+                        colorLock = true;
+                    }
+                    else if (colorLock == false)
+                    {
+                        choose_textbox[i].BackColor = Color.White;
+                    }
+                }
+            }
+        }
+    }
+}
